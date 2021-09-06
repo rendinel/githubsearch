ad897dd6485d0c927bf69b13343098cd3b4f06e1
rendinel <rendinel@protonmail.com>
2021-09-06 18:54:23 +0200

    push

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+//wrap all inside router,then we wrap all inside switch so only the first child with the to will be valid(es.dashboard will be displayed only on / not on login and error,without switch we will display every component on / because they include / on the path),then we setup the route for home login and error
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact={true}>
+          <Dashboard></Dashboard>
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='*'>
+          <Error></Error>
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/pages/Error.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+//simply display of the error page with a styled component and a link for the home
+const Error = () => {
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>sorry, the page you tried cannot be found</h3>
+        <Link className='btn' to='/'>
+          back home
+        </Link>
+      </div>
+    </Wrapper>
+  )
+}
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background: var(--clr-primary-10);
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    color: var(--clr-grey-3);
+    margin-bottom: 1.5rem;
+  }
+`
+export default Error
